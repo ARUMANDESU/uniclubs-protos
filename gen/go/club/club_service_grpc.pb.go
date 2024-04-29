a8ca0313614c05891e4312f154cf753c3b62ad3b
ARUMANDESU <zh.arumandes@gmail.com>
2024-04-29 12:29:36 +0500

    transfer club ownership

--- gen/go/club/club_service_grpc.pb.go
+++ gen/go/club/club_service_grpc.pb.go
@@ -51,6 +51,7 @@ type ClubClient interface {
 	BanMemberFromClub(ctx context.Context, in *BanMemberFromClubRequest, opts ...grpc.CallOption) (*empty.Empty, error)
 	UnbanUserFromClub(ctx context.Context, in *UnbanUserFromClubRequest, opts ...grpc.CallOption) (*empty.Empty, error)
 	ListBannedUsers(ctx context.Context, in *ListBannedUsersRequest, opts ...grpc.CallOption) (*ListBannedUsersResponse, error)
+	TransferOwnership(ctx context.Context, in *TransferOwnershipRequest, opts ...grpc.CallOption) (*empty.Empty, error)
 }
 
 type clubClient struct {
@@ -313,6 +314,15 @@ func (c *clubClient) ListBannedUsers(ctx context.Context, in *ListBannedUsersReq
 	return out, nil
 }
 
+func (c *clubClient) TransferOwnership(ctx context.Context, in *TransferOwnershipRequest, opts ...grpc.CallOption) (*empty.Empty, error) {
+	out := new(empty.Empty)
+	err := c.cc.Invoke(ctx, "/club.Club/TransferOwnership", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // ClubServer is the server API for Club service.
 // All implementations must embed UnimplementedClubServer
 // for forward compatibility
@@ -345,6 +355,7 @@ type ClubServer interface {
 	BanMemberFromClub(context.Context, *BanMemberFromClubRequest) (*empty.Empty, error)
 	UnbanUserFromClub(context.Context, *UnbanUserFromClubRequest) (*empty.Empty, error)
 	ListBannedUsers(context.Context, *ListBannedUsersRequest) (*ListBannedUsersResponse, error)
+	TransferOwnership(context.Context, *TransferOwnershipRequest) (*empty.Empty, error)
 	mustEmbedUnimplementedClubServer()
 }
 
@@ -435,6 +446,9 @@ func (UnimplementedClubServer) UnbanUserFromClub(context.Context, *UnbanUserFrom
 }
 func (UnimplementedClubServer) ListBannedUsers(context.Context, *ListBannedUsersRequest) (*ListBannedUsersResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method ListBannedUsers not implemented")
+}
+func (UnimplementedClubServer) TransferOwnership(context.Context, *TransferOwnershipRequest) (*empty.Empty, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method TransferOwnership not implemented")
 }
 func (UnimplementedClubServer) mustEmbedUnimplementedClubServer() {}
 
@@ -953,6 +967,24 @@ func _Club_ListBannedUsers_Handler(srv interface{}, ctx context.Context, dec fun
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Club_TransferOwnership_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(TransferOwnershipRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ClubServer).TransferOwnership(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/club.Club/TransferOwnership",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ClubServer).TransferOwnership(ctx, req.(*TransferOwnershipRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // Club_ServiceDesc is the grpc.ServiceDesc for Club service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -1071,6 +1103,10 @@ var Club_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "ListBannedUsers",
 			Handler:    _Club_ListBannedUsers_Handler,
+		},
+		{
+			MethodName: "TransferOwnership",
+			Handler:    _Club_TransferOwnership_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
